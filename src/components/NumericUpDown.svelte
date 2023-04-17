93163fbe6f857faa47cb0414def7290b525d01be
jaxsonhwphotonics <105377810+jaxsonhwphotonics@users.noreply.github.com>
2023-04-17 15:48:28 -0400

    Start creating the inputs

---
+++ src/components/NumericUpDown.svelte
@@ -0,0 +1,54 @@
+<script>
+    let value = 0;
+
+	function increment() {
+		value += 1;
+	}
+    function decrement() {
+		if ( value > 0 )
+            value -= 1;
+	}
+</script>
+
+<div class="custom-number-input h-9 w-32">
+	<div class="flex flex-row h-9 w-full rounded-xl relative bg-transparent mt-0.5">
+		<button
+			data-action="decrement"
+            on:click={decrement}
+			class=" bg-gray-900 text-gray-600 hover:text-gray-300 hover:bg-gray-800 h-full w-20 rounded-l-lg cursor-pointer outline-none"
+		>
+			<span class="m-auto text-2xl font-thin">−</span>
+		</button>
+
+		<input
+			type="number"
+			class="outline-none focus:outline-none text-center w-full bg-gray-900 font-semibold text-md hover:text-gray-200 focus:text-white md:text-basecursor-default flex items-center text-gray-300"
+			name="custom-input-number"
+			bind:value={value}
+		/>
+
+		<button
+			data-action="increment"
+            on:click={increment}
+			class="bg-gray-900 text-gray-600 hover:text-gray-300 hover:bg-gray-800 h-full w-20 rounded-r-lg cursor-pointer"
+		>
+			<span class="m-auto text-2xl font-thin">+</span>
+		</button>
+	</div>
+</div>
+
+<style>
+	input[type='number']::-webkit-inner-spin-button,
+	input[type='number']::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+
+	.custom-number-input input:focus {
+		outline: none !important;
+	}
+
+	.custom-number-input button:focus {
+		outline: none !important;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,80 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import NumericUpDown from '../components/NumericUpDown.svelte';
+</script>
+
+<div class="h-screen w-full relative overflow-hidden text-gray-300 bg-gray-950 p-4">
+	<div class="flex row w-full">
+		<div class="text-3xl font-bold">Spendable</div>
+	</div>
+
+	<div class="w-full h-1 rounded-sm bg-gray-900 my-3" />
+
+	<div class="flex row w-full mb-2 space-x-2">
+		<div>
+			<label for="price" class="block text-sm font-medium leading-6 text-gray-600"
+				>Hourly Wage</label
+			>
+			<div class="relative mt-0.5 rounded-md shadow-sm">
+				<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+					<span class="text-gray-500">$</span>
+				</div>
+				<input
+					type="text"
+					name="wage"
+					id="wage"
+					class="block w-[150px] rounded-lg border-0 py-1.5 pl-7 pr-10 placeholder:text-gray-700 text-gray-400 sm:leading-6 focus:outline-none focus:ring-2 focus:ring-gray-700 bg-gray-900"
+					placeholder="0.00"
+				/>
+			</div>
+		</div>
+		<div>
+			<label for="price" class="block text-sm font-medium leading-6 text-gray-600"
+				>Hours Per Week</label
+			>
+			<NumericUpDown />
+		</div>
+
+        <div>
+			<label for="price" class="block text-sm font-medium leading-6 text-gray-600"
+				>Income Tax %</label
+			>
+			<div class="relative mt-0.5 rounded-md shadow-sm">
+				
+				<input
+					type="text"
+					name="tax"
+					id="tax"
+					class="block w-[105px] rounded-lg border-0 py-1.5 pl-7 pr-10 placeholder:text-gray-700 text-gray-400 sm:leading-6 focus:outline-none focus:ring-2 focus:ring-gray-700 bg-gray-900"
+					placeholder="0.0"
+				/>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-6">
+					<span class="text-gray-500">%</span>
+				</div>
+			</div>
+		</div>
+		<div>
+			<label for="price" class="block text-sm font-medium leading-6 text-gray-600"
+				>Weekly Save %</label
+			>
+			<div class="relative mt-0.5 rounded-md shadow-sm">
+				
+				<input
+					type="text"
+					name="tax"
+					id="tax"
+					class="block w-[105px] rounded-lg border-0 py-1.5 pl-7 pr-10 placeholder:text-gray-700 text-gray-400 sm:leading-6 focus:outline-none focus:ring-2 focus:ring-gray-700 bg-gray-900"
+					placeholder="0.0"
+				/>
+                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-6">
+					<span class="text-gray-500">%</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.950);
+	}
+</style>
